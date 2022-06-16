7610ed4b288a71dbd1e95139542f3d9d45d399cd
kimmose1992 <kimmose1992@gmail.com>
2022-06-16 17:05:26 +0900

    유효성 체크 로직 추가, 기본값 및 비밀번호

--- basketball/src/main/resources/static/js/common.js
+++ basketball/src/main/resources/static/js/common.js
@@ -1,4 +1,28 @@
 /**
+ * JQuery
+ */
+jQuery.fn.serializeObject = function() {
+	var obj = null;
+	try {
+		if (this[0].tagName && this[0].tagName.toUpperCase() == "FORM") {
+			var arr = this.serializeArray();
+			if (arr) {
+				obj = {};
+				jQuery.each(arr, function() {
+					obj[this.name] = this.value;
+        		});
+          	}
+		}
+	} catch (e) {
+		alert(e.message);
+	} finally {
+		
+	}
+   
+	return obj;
+};
+
+/**
  * CSRF
  */
 var token = $("meta[name='_csrf']").attr("content");
