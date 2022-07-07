e41ceab522e68e0149da8b12d94c1c5b91c5c2a5
kimmose1992 <kimmose1992@gmail.com>
2022-07-07 09:00:49 +0900

    Junit 테스트 패키지 추가

---
+++ basketball/src/main/java/com/study/basketball/tt/cm/PasswordEncoderTest.java
@@ -0,0 +1,30 @@
+package com.study.basketball.tt.cm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @title	: Spring Security PasswordEncoder 테스트 클래스
+ * @author	: 김모세
+ * @create	: 2022.07.07
+ */
+class PasswordEncoderTest {
+
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}	
+	
+	@Test
+	void test() {
+		String password = "1234";
+		String passwordEncrypt = passwordEncoder().encode(password);
+		
+		System.out.println("===================================");
+		System.out.println("## password :: " + password);
+		System.out.println("## passwordEncrypt :: " + passwordEncrypt);
+		System.out.println("## matches :: " + passwordEncoder().matches(password, passwordEncrypt));
+		System.out.println("===================================");
+	}
+
+}
